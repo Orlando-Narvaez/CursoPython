cfebc8d7b92e42e2609ac6af987adf39d8efa9a6
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-13 11:15:48 -0500

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -6,18 +6,26 @@
 #imprimir_mensaje()
 #imprimir_mensaje()
 
-def conversacion(mensaje):
-    print("Hola")
-    print("Como estas?")
-    print(mensaje)
-    print("Adios")
+#def conversacion(mensaje):
+#    print("Hola")
+#    print("Como estas?")
+#    print(mensaje)
+#    print("Adios")
 
-opcion = int(input("Elige una opcion (1, 2, 3): "))
-if opcion == 1:
-    conversacion("Elegiste la opcion 1")
-elif opcion == 2:
-    conversacion("Elegiste la opcion 2")
-elif opcion == 3:
-    conversacion("Elegiste la opcion 3")
-else:
-    print("Escribiste una opcion incorrecta")
+#opcion = int(input("Elige una opcion (1, 2, 3): "))
+#if opcion == 1:
+#    conversacion("Elegiste la opcion 1")
+#elif opcion == 2:
+#    conversacion("Elegiste la opcion 2")
+#elif opcion == 3:
+#    conversacion("Elegiste la opcion 3")
+#else:
+#    print("Escribiste una opcion incorrecta")
+
+def suma(a, b):
+    print("Se suman 2 numeros ")
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1,4)
+print(sumatoria)
