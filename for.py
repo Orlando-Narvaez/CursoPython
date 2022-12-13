8eb5b78095d47d77952a391174d3a7d9fd4499c0
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-13 12:27:30 -0500

    Create for.py
    Uso del ciclo for

---
+++ for.py
@@ -0,0 +1,12 @@
+def run():
+    for contador in range(10):
+        print(contador)
+
+    print("Segundo for: ")
+    
+    for i in range(10):
+        print(11 * i)
+
+
+if __name__ == '__main__':
+    run()
