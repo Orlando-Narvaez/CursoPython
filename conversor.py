ad132ba2db05706eed371e946658a58942e3bd37
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-13 10:10:49 -0500

    Update conversor.py
    Se incluye lo aprendido de los condicionales al conversor de moneda

--- conversor.py
+++ conversor.py
@@ -1,7 +1,37 @@
-pesos =input("¿Cuantos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 4845.88
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dolares")
+menu = """
+Bienvenido al conversor de monedas 🤑
+
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opcion: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos =input("¿Cuantos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 4807.59
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+elif opcion == 2:
+    pesos =input("¿Cuantos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 171.77
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+elif opcion == 3:
+    pesos =input("¿Cuantos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 19.67
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+else:
+    print("Opcion incorrecta")
