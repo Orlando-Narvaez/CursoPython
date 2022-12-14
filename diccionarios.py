e759a2499c22c06df235090d4451925c8d37e081
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-14 15:49:27 -0500

    Create diccionarios.py

---
+++ diccionarios.py
@@ -0,0 +1,31 @@
+def run():
+    miDiccionario = {
+        "Llave1": 1,
+        "Llave2": 2,
+        "Llave3": 3,
+    }
+    #print(miDiccionario["Llave1"])
+    #print(miDiccionario["Llave2"])
+    #print(miDiccionario["Llave3"])
+
+    poblacionPaises = {
+        "Argentina": 44938712,
+        "Brasil": 210147125,
+        "Colombia": 50372424,
+    }
+
+    #print(poblacionPaises["Argentina"])
+
+    #for pais in poblacionPaises.keys():
+    #    print(pais)
+
+    #for pais in poblacionPaises.values():
+    #    print(pais)
+
+    for pais, poblacion in poblacionPaises.items():
+        print(pais + " Tiene " + str(poblacion) +  " Habitantes")    
+
+
+
+if __name__ == "__main__" : 
+    run()
