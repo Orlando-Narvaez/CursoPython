8c4bfd3ee6d809088b2902334b34a34b3c35c5d9
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-14 14:35:18 -0500

    Update adivina_el_numero.py

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -1,21 +1,30 @@
 import random
 
 def run():
+    print("""
+    Bienvenido al juego de adivinar el numero, el computador generara un
+    numero aletorio entre el 1 y el 100
+
+    Tienes 7 oportunidades de adivinar el numero
+    """)
     numerorandom = random.randint(1, 100)
     numeroelegido = int(input("Introduce un numero entre el 1 y el 100: "))
-    vidas = 5
-    while numerorandom != numeroelegido :
-        if numerorandom < numeroelegido : 
-            print("Elige un numero mas pequeño.")
+    print("\n")
+    vidas = 7
+    while numerorandom != numeroelegido:
+        if numerorandom < numeroelegido: 
+            print("Elige un numero mas pequeño\n")
             vidas -= 1
         elif numerorandom > numeroelegido : 
-            print("Elige un numero mas grande.")
+            print("Elige un numero mas grande\n")
             vidas -= 1
+
         if vidas == 0 :
             print("GAME OVER")
             break
-        print("Tienes", vidas, "vidas")
+        print("Tienes", vidas, "vidas\n")
         numeroelegido = int(input("Introduce numero: "))
+        print("\n")
     if numerorandom == numeroelegido : 
         print("FELICIDADES GANASTE")
 
