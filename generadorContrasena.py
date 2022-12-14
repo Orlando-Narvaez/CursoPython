2efdfb526a02d63d7f44f060989f8adf4c35b2cb
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-14 16:12:16 -0500

    Update generadorContrasena.py
    Se crea un código que genera contraseñas aleatorias

--- generadorContrasena.py
+++ generadorContrasena.py
@@ -1,1 +1,28 @@
 import random
+
+
+def generarContrasena():
+    mayusculas = ["A","B","C","D","E"]
+    minusculas = ["a","b","c","d","e"]
+    simbolos = ["!","·","$","&","/","(",")","="]
+    numeros = ["1","2","3", "4","5","6","7","8","9","0"]
+
+    caracteres = mayusculas + minusculas + simbolos + numeros
+
+    contrasena = []
+
+    for i in range(15):
+        caracter_randon = random.choice(caracteres)
+        contrasena.append(caracter_randon)
+
+    contrasena = "".join(contrasena)
+    return contrasena
+
+def run():
+    contrasena = generarContrasena()
+    print("Tu nueva contraseña es: " + contrasena)
+
+
+
+if __name__ == "__main__" : 
+    run()
